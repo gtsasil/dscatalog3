b4a0b4bb84a15db4645373c065a9ff5fe2197d0e
Gleisson Santos Silva <gtsasil@gmail.com>
2022-02-17 23:19:34 +0000

    "Data Base Seeding"

--- src/main/java/com/gtsasil/dscatalog3/services/CategoryService.java
+++ src/main/java/com/gtsasil/dscatalog3/services/CategoryService.java
@@ -4,6 +4,7 @@ import com.gtsasil.dscatalog3.entities.Category;
 import com.gtsasil.dscatalog3.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,6 +14,7 @@ public class CategoryService {
     @Autowired
     public CategoryRepository repository;
 
+    @Transactional(readOnly = true)
     public List<Category> findAll(){
         return repository.findAll();
     }
