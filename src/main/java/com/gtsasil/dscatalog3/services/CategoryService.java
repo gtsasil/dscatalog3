8ad9f7870f7a2b53b4dc848c5262aaa07fcec998
Gleisson Santos Silva <gtsasil@gmail.com>
2022-02-22 17:14:56 +0000

    "Update category"

--- src/main/java/com/gtsasil/dscatalog3/services/CategoryService.java
+++ src/main/java/com/gtsasil/dscatalog3/services/CategoryService.java
@@ -3,11 +3,12 @@ package com.gtsasil.dscatalog3.services;
 import com.gtsasil.dscatalog3.dto.CategoryDTO;
 import com.gtsasil.dscatalog3.entities.Category;
 import com.gtsasil.dscatalog3.repositories.CategoryRepository;
-import com.gtsasil.dscatalog3.services.exceptions.EntityNotFoundException;
+import com.gtsasil.dscatalog3.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -26,10 +27,32 @@ public class CategoryService {
         // or can be method reference **return list.stream().map(CategoryDTO::new).collect(Collectors.toList());
     }
 
+    @Transactional(readOnly = true)
     public CategoryDTO findById(Long id) {
         Optional<Category> obj = repository.findById(id);
-        Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity Not Found"));
+        Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity Not Found"));
 
         return new CategoryDTO(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public CategoryDTO insert(CategoryDTO dto) {
+        Category entity = new Category();
+        entity.setName(dto.getName());
+        entity = repository.save(entity);
+
+        return new CategoryDTO(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public CategoryDTO update(Long id, CategoryDTO dto) {
+        try {
+            Category entity = repository.getOne(id);
+            entity.setName(dto.getName());
+            entity = repository.save(entity);
+            return new CategoryDTO(entity);
+        }catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException("Id not found" +id);
+        }
     }
 }
--- src/main/java/com/gtsasil/dscatalog3/services/exceptions/EntityNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.gtsasil.dscatalog3.services.exceptions;
-
-public class EntityNotFoundException extends RuntimeException{
-
-    public EntityNotFoundException(String msg) {
-        super(msg);
-    }
-}
